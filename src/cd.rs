5d87b00ead5eb22b669c717e381e7460939e12ad
Stary Leń <oliwiernowicki4@gmail.com>
2024-12-08 15:25:09 +0100

    Track cd.rs

---
+++ src/cd.rs
@@ -0,0 +1,13 @@
+use std::env;
+use std::process;
+use carrot_libs::args;
+
+fn main() {
+    let opts = args::opts();
+    let swcs = args::swcs();
+    if !opts.is_empty() || !swcs.0.is_empty() || !swcs.1.is_empty() {
+        eprintln!("This program does not support any options nor switches!");
+        process::exit(1);
+    }
+    println!("{}", env::current_dir().unwrap().display())
+}
