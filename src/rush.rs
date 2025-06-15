e267994ab66fc9c21e654c5510bb78a108d36240
Stary Leń <oliwiernowicki4@gmail.com>
2025-06-15 10:05:23 +0200

    Prepare to start work over

--- src/rush.rs
+++ src/rush.rs
@@ -19,24 +19,6 @@ use global::{
     set_interrupt_now,
 };
 
-#[derive(Debug)]
-enum ShellModes {
-    // Run all commands inside IF/ELSEIF/ELSE block
-    CmpSuccess,
-    // Skip tasks inside current IF/ELSEIF/ELSE block and try running another one
-    CmpFailure,
-    // When shell mode is set to CmpSuccess, skip checking any other IF/ELSEIF/ELSE and included subcommands
-    // IfDone status will be reset by ENDIF keywords
-    IfDone,
-    // After you reach "endlock", go back to LOCK defined in position_of_lock.
-    // Allow usage of BREAK and CONTNUE
-    Lock(usize),
-    // Skip executing commands until you reach ENDLOCK. Go back to LOCK.
-    LockContinue(usize),
-    // Skip executing commands until you reach ENDLOCK. But do not go back to LOCK.
-    LockFree,
-}
-
 fn main() {
     // Get options and switches
     let opts = args::opts();
@@ -270,318 +252,37 @@ fn make_script_thread(script: Vec<String>) {
     set_allow_interrupts(false);
 }
 
+#[derive(Debug)]
+enum ShellMode {
+    // Run all commands inside IF/ELSEIF/ELSE block
+    CmpSuccess,
+    // Skip tasks inside current IF/ELSEIF/ELSE block and try running another one
+    CmpFailure,
+    // When shell mode is set to CmpSuccess, skip checking any other IF/ELSEIF/ELSE and included subcommands
+    // IfDone status will be reset by ENDIF keywords
+    IfDone,
+    // After you reach "endlock", go back to LOCK defined in position_of_lock.
+    // Allow usage of BREAK and CONTNUE
+    Lock,
+    // Skip executing commands until you reach ENDLOCK. Go back to LOCK.
+    LockContinue,
+    // Skip executing commands until you reach ENDLOCK. But do not go back to LOCK.
+    LockFree,
+}
+
+struct ShellModes {
+    list: Vec<ShellModes>
+}
+
 fn run_script(script: Vec<String>) {
-    // Iterate through words in the script and slowly collect them to a buffer
-    let mut buf: Vec<String> = Vec::new();
-    // Count line numbers in the script for better error messages
-    let mut line_number = 1;
-    // How many nested blocks do we have?
-    let mut nesting_level: usize = 0;
-
-    // This variable defines status of the execution mode
-    // We'll skip some elseif's and run loops over and over again based on the value of this variable.
-    // More about this at the beginning of this source file.
-    let mut shell_mode: Vec<ShellModes> = Vec::new();
-
-    // Until we reach the end of the script
-    while index() < script.len() {
-        if interrupt_now() {
-            return;
-        }
-        // Shorthand for currently iterated word of a script
-        let w = script[index()].clone();
-        // Is this the last word? The script is ending!
-        let the_last_word_in_script = index() == script.len() - 1;
-        // Add currently iterated word to a temporary buffer
-        if !w.ends_with("\\;") && w.ends_with(';') {
-            buf.push(w.strip_suffix(';').unwrap().to_string());
-        } else {
-            buf.push(w.clone());
-        };
-
-        let contains_if_done = shell_mode.iter().any(|s| matches!(s, ShellModes::IfDone));
-        let contains_cmp_failure = shell_mode.iter().any(|s| matches!(s, ShellModes::CmpFailure));
-        let contains_lock_free = shell_mode.iter().any(|s| matches!(s, ShellModes::LockFree));
-        let contains_lock_continue = shell_mode
-            .iter()
-            .any(|s| matches!(s, ShellModes::LockContinue(_)));
-
-        let block_execution =
-            contains_if_done ||
-            contains_cmp_failure ||
-            contains_lock_free ||
-            contains_lock_continue;
-
-
-        // If we reach the end of a script OR some command separator like '\n' or ';' is found...
-        if the_last_word_in_script || (!w.ends_with("\\;") && w.ends_with(';')) || w.ends_with('\n')
-        // || (buf.len() < 2 && w == "lock")
-        // || (buf.len() < 2 && w == "if")
-        //
-        // ... try running the command ...
-        {
-            dbg!(&buf);
-
-            // First argument in the buffer (buf[0]) is a program name
-            // Check whether it's something built into the shell or not.
-            let program_name = buf[0].clone();
-            if !block_execution {
-                match remove_quotationmarks(&program_name).as_str() {
-                    /*
-                    marbulec was here
-                    dito
-
-                    When we reach the LOCK keyword, we have to remember it's position and set working shell_mode to 'Lock'
-                    When lock mode is enabled, Rush will execute all commands inside of an lock block until it reaches END keyword.
-                    Then, Rush will jump back to the position of LOCK and execute all of the commands again and again and again...
-                    It's an endless loop.
-
-                    But we can break out of this and set us free with the FREE keyword.
-                    When we approach it, we'll set the shell_mode to LockFree and then, skip executing any command until END is found.
-
-                    Additionally, you can rerun the loop from the beggining, skipping any following command.
-                    This can be done using CONTINUE.
-                    CONTINUE will change shell_mode to LockContinue, stop the execution of any command until END and then, run LOCK again.
-                    */
-                    "lock" => {
-                        // Increment nesting count
-                        nesting_level += 1;
-
-                        // Set shell_mode to Lock and save it's position
-                        let position_of_program_name_in_script = index();
-                        shell_mode.push(ShellModes::Lock(position_of_program_name_in_script));
-                    }
-                    /*
-                    set val 0
-                    lock
-                        set val 5
-                        if $val = 5;
-                            free
-                            say "Don't run it"
-                        endif
-                        say "This can't be seen"
-                    endlock
-
-                    In the example below, FREE or CONTINUE keyword may be used when we're not in the LOCK directly.
-                    Therefore, we need to look back for the LAST possible shell_mode with Lock value and change it to LockFree/LockContinue
-
-                    To prevent execution of any command after FREE/CONTINUE (doesn't matter if it's still an IF stmt. or smth. outside of it in LOCK itself),
-                    shell must remember that there is at least one LockFree/LockContinue value
-                    */
-                    "free" => {
-                        let mut position_of_found_lock_mode_in_shellmodes = 0;
-                        // Check if we are in a LOCK mode somewhere in shell_mode list (look for 'Lock' mode from the end of a list)
-                        let inlock = shell_mode.iter().rev().enumerate().any(|s| {
-                            let a = s.1;
-                            position_of_found_lock_mode_in_shellmodes =
-                                (&shell_mode.len() - 1) - s.0;
-                            matches!(a, ShellModes::Lock { .. })
-                        });
-                        // If we are in a LOCK, change latest Lock to a LockFree.
-                        if inlock {
-                            shell_mode[position_of_found_lock_mode_in_shellmodes] =
-                                ShellModes::LockFree;
-                        }
-                        // If we are NOT in a LOCK in any way
-                        else {
-                            let e = ("Usage of \"FREE\" is not permited outside of the \"LOCK\" statement.").to_string();
-                            print_err(e, program_name.clone(), line_number)
-                        }
-                    }
-                    "continue" => {
-                        let mut position_of_found_lock_mode_in_shellmodes = 0;
-                        // Check if we are in a LOCK mode somewhere in shell_mode list (look for 'Lock' mode from the end of a list)
-                        let inlock = shell_mode.iter().rev().enumerate().any(|s| {
-                            let a = s.1;
-                            position_of_found_lock_mode_in_shellmodes =
-                                (&shell_mode.len() - 1) - s.0;
-                            matches!(a, ShellModes::Lock { .. })
-                        });
-
-                        // If we are in a LOCK, change latest Lock(position in script) shell_mode to a LockContinue(position in script).
-                        if inlock {
-                            match shell_mode[position_of_found_lock_mode_in_shellmodes] {
-                                ShellModes::Lock(number) => {
-                                    shell_mode[position_of_found_lock_mode_in_shellmodes] =
-                                        ShellModes::LockContinue(number);
-                                }
-                                _ => {
-                                    unreachable!("Program's logic contradics itself! Please, report this error!");
-                                }
-                            }
-                        }
-                        // If we are NOT in a LOCK in any way
-                        else {
-                            let e = ("Usage of \"CONTINUE\" is not permited outside of the \"LOCK\" statement.").to_string();
-                            print_err(e, program_name.clone(), line_number)
-                        }
-                    }
-
-                    "if" => {
-                        // Increment nesting count
-                        nesting_level += 1;
-
-                        // Set shell_mode to Lock and save it's position
-                        let position_of_program_name_in_script = index();
-                        let out = r#if::logic(buf.clone());
-                        match out {
-                            Ok(true) => shell_mode.push(ShellModes::CmpSuccess),
-                            Ok(false) => shell_mode.push(ShellModes::CmpFailure),
-                            Err(e) => print_err(e, program_name.clone(), line_number),
-                        };
-                    }
-                    "elseif" | "eif" => {
-                        dbg!(&shell_mode[nesting_level]);
-                        match shell_mode[nesting_level] {
-                            // Is it running after unsuccessfull IF/ELSEIF?
-                            ShellModes::CmpFailure => {
-                                // Set shell_mode to Lock and save it's position
-                                let position_of_program_name_in_script = index();
-                                let out = r#if::logic(buf.clone());
-                                match out {
-                                    Ok(true) => shell_mode.push(ShellModes::CmpSuccess),
-                                    Ok(false) => shell_mode.push(ShellModes::CmpFailure),
-                                    Err(e) => print_err(e, program_name.clone(), line_number),
-                                };
-                            }
-                            ShellModes::CmpSuccess => {
-                                shell_mode[nesting_level] = ShellModes::IfDone
-                            }
-                            ShellModes::IfDone => (),
-                            _ => {
-                                let e =
-                                    "Usage of \"ELSEIF\" outside of an IF statement is incorrect"
-                                        .to_string();
-                                print_err(e, program_name.clone(), line_number);
-                            }
-                        };
-                    }
-                    "else" => {
-                        todo!();
-                    }
-
-                    "set" => {
-                        if let Err(e) = variables::setenv(&buf) {
-                            print_err(e, program_name.clone(), line_number);
-                        };
-                    }
-                    "rem" => {
-                        if let Err(e) = variables::remenv(&buf) {
-                            print_err(e, program_name.clone(), line_number);
-                        };
-                    }
-                    "get" => match variables::getenv(&buf) {
-                        Ok(res) => println!("{res}"),
-                        Err(e) => print_err(e, program_name.clone(), line_number),
-                    },
-                    "++" => {
-                        if let Err(e) = variables::chenv(&buf, true) {
-                            print_err(e, program_name.clone(), line_number);
-                        };
-                    }
-                    "--" => {
-                        if let Err(e) = variables::chenv(&buf, false) {
-                            print_err(e, program_name.clone(), line_number);
-                        };
-                    }
-
-                    "gt" => {
-                        if let Err(e) = directories::gt(&buf) {
-                            print_err(e, program_name.clone(), line_number);
-                        };
-                    }
-
-                    "panic" => {
-                        panic!("User invoked panic");
-                    }
-                    "exit" => {
-                        process::exit(0);
-                    }
-
-                    // Comments will never be run
-                    "#" | "endif" | "endlock" => {},
-                    // Not built in?
-                    _ => {
-                            if let Err(e) = exec::exec(&buf) {
-                                print_err(e, program_name.clone(), line_number);
-                            }
-                            // The fuck was this code about
-                            //
-                            // let mut resolved_buf = resolver(buf.iter().collect(), true, true);
-                            // match resolved_buf {
-                            //     Err(e) => print_err(e, program_name, line_number),
-                            //     Ok(gg) => exec::exec(&gg.split_whitespace()),
-                            // }
-                    }
-                };
-            }
-            // ... and finally, after command is done, clear the buffer
-            buf.clear();
-
-            match program_name.as_str() {
-                "endlock" => {
-                    let mut position_of_found_lock_mode_in_shellmodes = 0;
-                    // Check if we are in a LOCK mode somewhere in shell_mode list (look for 'Lock' mode from the end of a list)
-                    let inlock = shell_mode.iter().rev().enumerate().any(|s| {
-                        let a = s.1;
-                        position_of_found_lock_mode_in_shellmodes = (&shell_mode.len() - 1) - s.0;
-                        matches!(a, ShellModes::Lock { .. })
-                    });
-                    if inlock {
-                        match shell_mode[nesting_level-1] {
-                            // If current shell_mode is set to Lock or LockContinue,
-                            // go back to the position of LOCK so we'll execute it again as intended.
-                            ShellModes::Lock(a) | ShellModes::LockContinue(a) => {
-                                set_index(position_of_found_lock_mode_in_shellmodes);
-                                shell_mode[nesting_level] = ShellModes::Lock(a);
-                            }
-                            // Set free from LOCK loop
-                            ShellModes::LockFree => {
-                                shell_mode.pop();
-                                nesting_level -= 1;
-                            }
-                            _ => {
-                                let e = "Usage of \"ENDLOCK\" in wrong nesting".to_string();
-                                print_err(e, program_name, line_number);
-                            }
-                        }
-                    }
-                },
-                "endif" => {
-                    let mut position_of_found_if_mode_in_shellmodes = 0;
-                    // Check if we are in a LOCK mode somewhere in shell_mode list (look for 'Lock' mode from the end of a list)
-                    let inif = shell_mode.iter().rev().enumerate().any(|s| {
-                        let a = s.1;
-                        position_of_found_if_mode_in_shellmodes = (&shell_mode.len() - 1) - s.0;
-                        matches!(a, ShellModes::CmpFailure)
-                        || matches!(a, ShellModes::CmpSuccess)
-                        || matches!(a, ShellModes::IfDone)
-                    });
-                    if inif {
-                        match shell_mode[nesting_level-1] {
-                            ShellModes::CmpSuccess | ShellModes::CmpFailure | ShellModes::IfDone => {
-                                shell_mode.pop();
-                                nesting_level -= 1;
-                            }
-                            _ => {
-                                let e = "Usage of \"ENDIF\" in wrong nesting".to_string();
-                                print_err(e, program_name, line_number);
-                            }
-                        }
-                    }
-                },
-                _ => (),
-            };
-        }
-
-        // Bump line number if we find a new line character
-        if w.ends_with('\n') && !w.ends_with("\\n") {
-            line_number += 1;
-        }
-
-        // Bump index number
-        set_index(index() + 1);
-    }
-    // When you stop working with the buffer, reset indexer
-    set_index(0);
+    // When looking for words for a command that we are starting to work with,
+    // where should we start searching from?
+    let mut start_search_from = 0;
+    
+    // Create a buffer that will hold words from the script **that we are currently working on**.
+    let buf = script[start_search_from..].iter().position(
+        |x| !x.ends_with("\\;") && (x.ends_with(';') || x.ends_with('\n'))
+    );
+    start_search_from += buf.len();
+    
 }
